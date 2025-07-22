cba678c0614757fec641e06aaef56225e0ad4e70
knightsdd <gorvas53@yandex.ru>
2025-07-22 09:09:43 +0300

    User chi server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,16 +5,32 @@ import (
 
 	"github.com/knightsdd/shorturl2/internal/handlers"
 	"github.com/knightsdd/shorturl2/internal/storage"
+
+	"github.com/go-chi/chi/v5"
 )
+
+// example with only net http mux server
+// func main() {
+// 	urlStorage := storage.GetStorage()
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/{$}", handlers.GenShortUrl(urlStorage))
+// 	mux.HandleFunc("GET /{prefix}/{$}", handlers.GetOriginalUrl(urlStorage))
+
+// 	err := http.ListenAndServe(`:8080`, mux)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+func MainRouter(storage storage.UrlStorage) chi.Router {
+	r := chi.NewRouter()
+	r.Post("/{$}", handlers.GenShortUrl(storage))
+	r.Get("/{prefix}/{$}", handlers.GetOriginalUrl(storage))
+	return r
+}
 
 func main() {
 	urlStorage := storage.GetStorage()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", handlers.GenShortUrl(urlStorage))
-	mux.HandleFunc("GET /{prefix}/{$}", handlers.GetOriginalUrl(urlStorage))
-
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
-	}
+	r := MainRouter(urlStorage)
+	http.ListenAndServe(":8080", r)
 }
