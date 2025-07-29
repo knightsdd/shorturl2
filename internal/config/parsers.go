7a64040a0c79583c1bdddb6e4330f2412e7f856a
knightsdd <gorvas53@yandex.ru>
2025-07-29 09:56:20 +0300

    Add env params. Fix readme

--- internal/config/parsers.go
+++ internal/config/parsers.go
@@ -2,8 +2,13 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
+
+const trimChars string = "/:,. "
 
 func ParseServerFlags() {
 	flag.StringVar(&ServerHost, "host", "localhost", "Server run host")
@@ -11,11 +16,30 @@ func ParseServerFlags() {
 	flag.StringVar(&ServerEndpoint, "endpoint", "http://localhost:8080/", "Server endpoint basic url")
 	flag.Parse()
 
-	ServerEndpoint = strings.Trim(ServerEndpoint, "/:,. ") + "/"
+	if host := os.Getenv("SERVER_HOST"); host != "" {
+		ServerHost = host
+	}
+	if port := os.Getenv("SERVER_PORT"); port != "" {
+		port, err := strconv.Atoi(port)
+		if err == nil {
+			ServerPort = port
+		}
+	}
+	if endpoint := os.Getenv("SERVER_ENDPOINT"); endpoint != "" {
+		ServerEndpoint = endpoint
+	}
+
+	ServerEndpoint = strings.Trim(ServerEndpoint, trimChars) + "/"
 }
 
 func ParseClientFlags() {
 	flag.StringVar(&ClientEndpoint, "endpoint", "http://localhost:8080/", "Client endpoint basic url")
 	flag.Parse()
-	ClientEndpoint = strings.Trim(ClientEndpoint, "/:,. ") + "/"
+
+	if endpoint := os.Getenv("CLIENT_ENDPOINT"); endpoint != "" {
+		fmt.Println("ECHO", endpoint)
+		ClientEndpoint = endpoint
+	}
+
+	ClientEndpoint = strings.Trim(ClientEndpoint, trimChars) + "/"
 }
